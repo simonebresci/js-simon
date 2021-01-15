5f32f53f4f7bb3dbd38ec8e56b72afd264e74c3a
Simone <simone.bresci.05@gmail.com>
2021-01-15 15:08:10 +0100

    inizio progetto

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Esercizio di oggi: Simon Says
+// repo/cartella js-simon
+// Un alert espone 5 numeri casuali diversi.
+// Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+// Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+
+
+$(document).ready(function(){
+  alert('Simon says');
+//
+//
+//   $('.navbar-main .menu .link').click(function(){
+//     // Chiudi tutti i dropdown
+//     $('.navbar-main .menu .link .dropdown-box').css('display','none');
+//
+//     // Apri/chiudi menu dropdown quand link viene cliccato
+//     $(this).find('.dropdown-box').fadeToggle();
+//   })
+//
+//
+});
